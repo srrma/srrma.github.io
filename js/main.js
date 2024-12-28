d59fc4946bbfb6f3b9c69d2fb65a30b0dd8132d5
srrma <148332949+srrma@users.noreply.github.com>
2024-12-28 15:54:32 +0700

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Initialize Masonry
-    var grid = document.querySelector('.grid');
-    var msnry = new Masonry(grid, {
-        itemSelector: '.grid-item',
-        columnWidth: '.grid-item',
-        percentPosition: true
-    });
-});
